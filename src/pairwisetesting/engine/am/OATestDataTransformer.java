bc98e68ed0a63c44cf33ed5fde183d9a93295c07
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-04-07 07:19:40 +0000

    Add method getMaxNumOfLevels to class MetaParameter Make OATestDataTransformer support missing values Add test cases for AMEngine to verify that it can handle missing values correctly

--- src/pairwisetesting/engine/am/OATestDataTransformer.java
+++ src/pairwisetesting/engine/am/OATestDataTransformer.java
@@ -1,5 +1,6 @@
 package pairwisetesting.engine.am;
 
+import pairwisetesting.coredomain.Factor;
 import pairwisetesting.coredomain.ITestDataTransformer;
 import pairwisetesting.coredomain.MetaParameter;
 
@@ -8,9 +9,23 @@ public class OATestDataTransformer implements ITestDataTransformer {
 	public String[][] transform(MetaParameter mp, String[][] rawTestData) {
 		String[][] testData = rawTestData;
 		for (int columnIndex = 0; columnIndex < rawTestData[0].length; columnIndex++) {
+			
 			String factorName = mp.getFactorNames()[columnIndex];
+			Factor factor = mp.getFactor(factorName);
+			
+			// start index of the level to fill the missing value
+			int nextIndexForEmpty = 0;
+			
 			for (int rowIndex = 0; rowIndex < rawTestData.length; rowIndex++) {
-				testData[rowIndex][columnIndex] = mp.getLevelOfFactor(factorName, Integer.parseInt(rawTestData[rowIndex][columnIndex]) - 1);
+				int index = Integer.parseInt(rawTestData[rowIndex][columnIndex]) - 1;
+				if (index < factor.getNumOfLevels()) {
+					testData[rowIndex][columnIndex] = factor.getLevel(index);
+				} else {
+					testData[rowIndex][columnIndex] = factor.getLevel(nextIndexForEmpty);
+					// next index of the level to fill the missing value
+					nextIndexForEmpty = (nextIndexForEmpty + 1) % factor.getNumOfLevels();
+				}
+					
 			}
 		}
 		return testData;
