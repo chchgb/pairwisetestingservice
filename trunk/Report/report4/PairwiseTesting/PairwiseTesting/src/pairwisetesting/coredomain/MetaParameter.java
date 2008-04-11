70fd83a501a5bf331cd683943f7cec72e3e44a79
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-04-11 05:37:47 +0000

    Report

---
+++ trunk/Report/report4/PairwiseTesting/PairwiseTesting/src/pairwisetesting/coredomain/MetaParameter.java
@@ -0,0 +1,55 @@
+package pairwisetesting.coredomain;
+
+import java.util.LinkedHashMap;
+
+public class MetaParameter {
+
+	private int strength = 2;
+	private LinkedHashMap<String, Factor> factorMap = new LinkedHashMap<String, Factor>();
+
+	public MetaParameter(int strength) {
+		this.strength = strength;
+	}
+
+	public MetaParameter() {
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void addFactor(Factor f1) {
+		factorMap.put(f1.getName(), f1);
+	}
+
+	public Factor getFactor(String factorName) {
+		return factorMap.get(factorName);
+	}
+
+	public Factor[] getFactors() {
+		return factorMap.values().toArray(new Factor[0]);
+	}
+
+	public String[] getFactorNames() {
+		return factorMap.keySet().toArray(new String[0]);
+	}
+
+	public String getLevelOfFactor(String factorName, int index) {
+		return getFactor(factorName).getLevel(index);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == this)
+			return true;
+		if (!(other instanceof MetaParameter))
+			return false;
+		MetaParameter mp = (MetaParameter) other;
+		return (strength == mp.strength) && (factorMap.equals(mp.factorMap));
+	}
+
+}
