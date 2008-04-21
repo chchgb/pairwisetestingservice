2dd66a762428b367a9c79a40796a03e712ed0f7b
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-04-21 04:42:19 +0000

    Add experiments about StringTemplete and Java Compiler API to show the Test Workflow

---
+++ trunk/experiments/StringTemplate/Arg.java
@@ -0,0 +1,7 @@
+class Arg {
+    String name;
+    String type;
+    public Arg(String name, String type) {this.name=name; this.type=type;}
+    public String getName() {return name;}
+    public String getType() {return type;}
+}
