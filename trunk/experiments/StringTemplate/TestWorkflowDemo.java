2861a13cee0e5e8731cae1dc49d7d978cd411c00
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-04-21 13:14:16 +0000

    Add some info for TestWorkflowDemo

--- trunk/experiments/StringTemplate/TestWorkflowDemo.java
+++ trunk/experiments/StringTemplate/TestWorkflowDemo.java
@@ -36,15 +36,17 @@ public class TestWorkflowDemo {
 		query.setAttribute("args", new Arg("upper", "int"));
 		query.setAttribute("returnType", "boolean");
 		TextFile.write("src/demo/UnitTest.java", query.toString());
+		System.out.println("Test case was generated.");
 		
 		// Compile the classes related with test
 		String[] files = new String[]{"src/demo/UnitTest.java", "src/demo/Util.java"};
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
 		Iterable<? extends JavaFileObject> compilationUnits1 = fileManager.getJavaFileObjects(files);
-		String[] options = new String[] {"-d", "bin", "-cp", "d:/testng-5.7-jdk15.jar"};
+		String[] options = new String[] {"-d", "bin"};
 		compiler.getTask(null, fileManager, null, Arrays.asList(options), null, compilationUnits1).call();
 		fileManager.close();
+		System.out.println("Test case was compiled.");
 		
 		// Load the classes related with test
 		File outputDir = new File("bin");  
@@ -52,15 +54,15 @@ public class TestWorkflowDemo {
 		URL[] urls = new URL[]{outputDir.toURI().toURL()};  
 		URLClassLoader ucl = new URLClassLoader(urls, cl);  
 		Class clazz = ucl.loadClass("demo.UnitTest"); 
+		System.out.println("Test case class was loaded.");
 		
 		// Run Test with TestNG
+		System.out.println("Run Test case with TestNG...");
 		TestNG tng = new TestNG();
 		tng.setTestClasses(new Class[] { clazz });
 		TestListenerAdapter listener = new TestListenerAdapter();
 		tng.addListener(listener);
 		tng.run();
 		System.out.println("PASSED: " + listener.getPassedTests().size());
-
 	}
-
 }
