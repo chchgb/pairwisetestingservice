9d82d74ceec9360733fa14115bfebdd086a61fbc
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-04-27 09:32:30 +0000

    Find dependencies of each method. By Alex Wang ^_^

--- src/pairwisetesting/dependency/DepFinder.java
+++ src/pairwisetesting/dependency/DepFinder.java
@@ -18,8 +18,9 @@ public class DepFinder {
 		
 		String path = "bin\\pairwisetesting\\test";
 		String name = "TestPairwiseTesting";
+		String func = "test";
 		
-		df.findDependency(path, name);
+		df.findMethodDependency(path, name, func);
 	}
 	
 	/**
@@ -30,13 +31,6 @@ public class DepFinder {
 		outputFileName.append(classPath).append("\\");
 		outputFileName.append(className).append(".xml");
 		
-		StringBuilder extCommand = new StringBuilder();
-		extCommand.append("DependencyExtractor.bat -xml -out ");
-		extCommand.append(outputFileName).append(" ");
-		extCommand.append(classPath).append("\\");
-		extCommand.append(className).append(".class");
-		
-		extract(extCommand.toString());
 		c2c(className, outputFileName.toString());
 		c2p(outputFileName.toString());
 		p2p(outputFileName.toString());
@@ -44,13 +38,33 @@ public class DepFinder {
 		return null;
 	}
 	
+	public String findMethodDependency(String classPath, String className, String methodName){
+		StringBuilder classFileName = new StringBuilder();
+		classFileName.append(classPath).append("\\").append(className).append(".class");
+		
+		StringBuilder xmlFileName = new StringBuilder();
+		xmlFileName.append(classPath).append("\\").append(className).append(".xml");
+		
+		extract(classFileName.toString(), xmlFileName.toString());
+		f2f(xmlFileName.toString(), methodName);
+		return null;
+	}
+	
 	/**
 	 *  Call DependencyExtractor.bat
 	 *  to generate a XML file
 	 */
-	private void extract(String command){
-		System.out.println(command);
+	private void extract(String classFileName, String xmlFileName){
+		StringBuilder extCommand = new StringBuilder();
+		extCommand.append("DependencyExtractor.bat -xml -out ");
+		extCommand.append(xmlFileName).append(" ");
+		extCommand.append(classFileName);
+		
+		
 		try {
+			String command = extCommand.toString();
+			System.out.println(command);
+			
 			Process p = Runtime.getRuntime().exec(command);
 			
 			// Wait for p to end
@@ -66,12 +80,12 @@ public class DepFinder {
 	 * Call c2c.bat to handle XML file
 	 * Get Class-to-Class dependencies
 	 */
-	private void c2c(String className, String fileName){
+	private void c2c(String className, String xmlFileName){
 		// Command line for c2c
 		StringBuilder c2cCommand = new StringBuilder();
 		c2cCommand.append("c2c.bat -scope-includes ");
 		c2cCommand.append("\\").append(className).append("\\ ");
-		c2cCommand.append(fileName);
+		c2cCommand.append(xmlFileName);
 		
 		try {
 			String command = c2cCommand.toString();
@@ -94,10 +108,10 @@ public class DepFinder {
 	 * Call c2p.bat to handle XML file
 	 * Get Class-to-Package dependencies
 	 */
-	private void c2p(String fileName){
+	private void c2p(String xmlFileName){
 		// Command line for c2p
 		StringBuilder c2pCommand = new StringBuilder();
-		c2pCommand.append("c2p.bat -show-outbounds ").append(fileName);
+		c2pCommand.append("c2p.bat -show-outbounds ").append(xmlFileName);
 		
 		try {
 			String command = c2pCommand.toString();
@@ -120,10 +134,10 @@ public class DepFinder {
 	 * Call p2p.bat to handle XML file
 	 * Get Package-to-Package dependencies
 	 */
-	private void p2p(String fileName){
+	private void p2p(String xmlFileName){
 		// Command line for p2p
 		StringBuilder p2pCommand = new StringBuilder();
-		p2pCommand.append("p2p.bat -show-outbounds ").append(fileName);
+		p2pCommand.append("p2p.bat -show-outbounds ").append(xmlFileName);
 		
 		try {
 			String command = p2pCommand.toString();
@@ -141,5 +155,34 @@ public class DepFinder {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	private void f2f(String xmlFileName, String methodName){
+		// Command for f2f
+		StringBuilder f2fCommand = new StringBuilder();
+		f2fCommand.append("f2f.bat -show-outbounds ");
+		
+		//f2fCommand.append("-scope-includes ");
+		//f2fCommand.append("/").append(className).append("/ ");
+		
+		f2fCommand.append("-feature-scope-includes ");
+		f2fCommand.append("/").append(methodName).append("/ ");
+		f2fCommand.append(xmlFileName);
+		
+		try {
+			String command = f2fCommand.toString();
+			System.out.println(command);
+			
+			Process p = Runtime.getRuntime().exec(command);
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+			
+			String str = null;
+			while(null != (str = in.readLine())){
+				System.out.println(str);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
