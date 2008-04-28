805447af548f0df725e7d7cf6f0f28f8fcd7ef99
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-04-28 04:26:37 +0000

    public void setSchemaPath(String schemaPath);

--- src/pairwisetesting/coredomain/IMetaParameterProvider.java
+++ src/pairwisetesting/coredomain/IMetaParameterProvider.java
@@ -5,5 +5,6 @@ import pairwisetesting.exception.MetaParameterException;
 public interface IMetaParameterProvider {
 
 	MetaParameter get() throws MetaParameterException;
+	public void setSchemaPath(String schemaPath);
 
 }
