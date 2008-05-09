dda5e5b0f16f40b141e7bc19635920b2d922ebff
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-05-09 15:18:27 +0000

    add log

--- trunk/SourceCode/TestNGService/src/testingngservices/core/TestWorkflow.java
+++ trunk/SourceCode/TestNGService/src/testingngservices/core/TestWorkflow.java
@@ -11,32 +11,30 @@ import java.io.*;
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
 
-import pairwisetesting.util.TestingMetaParameter;
+import com.sun.org.apache.commons.logging.Log;
+import com.sun.org.apache.commons.logging.LogFactory;
 
+import pairwisetesting.util.TestingMetaParameter;
 
 public class TestWorkflow {
 	private TestingMetaParameter testingMeta;
 	private String workPath;
-	
-	public TestWorkflow(TestingMetaParameter testingMeta){
+	private Log log = LogFactory.getLog(TestWorkflow.class);
+
+	public TestWorkflow(TestingMetaParameter testingMeta) {
 		this.testingMeta = testingMeta;
 		this.workPath = testingMeta.getEndPath();
 	}
-	
-	public String testWorkflow(){
-		
-		for(String fileName:testingMeta.getFileArray()){
-			System.out.println(fileName);
-		}
-		
-		 
+
+	public String testWorkflow() {
 		String result = null;
 		testingMeta.addFile("src/pairwisetesting/util/Converter.java");
-		testingMeta.addFile("src/pairwisetesting/test/expect/Expectation.java");
+		testingMeta.addFile("src/test/expect/Expectation.java");
 		testingMeta.addLib("testng-5.8-jdk15.jar");
 		testingMeta.addLib("xom-1.1.jar");
 		testingMeta.writeFiles();
-		//testingMeta.writeTestCase();
+		// testingMeta.writeTestCase();
+		log.info("Refactor the source code structor");
 		File outputDir = new File(testingMeta.getEndPath() + "bin");
 		outputDir.mkdirs();
 
@@ -47,7 +45,11 @@ public class TestWorkflow {
 				null, null, null);
 		Iterable<? extends JavaFileObject> compilationUnits1 = fileManager
 				.getJavaFileObjects(files);
-				String[] options = new String[] { "-d", testingMeta.getEndPath() + "bin","-classpath",testingMeta.getLibString()};
+		String[] options = new String[] { "-d",
+				testingMeta.getEndPath() + "bin", "-classpath",
+				testingMeta.getLibString() };
+		
+		log.info("Compile source code");
 		compiler.getTask(null, fileManager, null, Arrays.asList(options), null,
 				compilationUnits1).call();
 		try {
@@ -58,34 +60,37 @@ public class TestWorkflow {
 		}
 
 		// Load the classes related with test
-		
+
 		ClassLoader cl = Thread.currentThread().getContextClassLoader();
 		URL[] urls;
 		try {
 			urls = new URL[] { outputDir.toURI().toURL() };
 			URLClassLoader ucl = new URLClassLoader(urls, cl);
-			
+
 			// Run Test with TestNG
-			//testingMeta.gettestCaseClassName()
+			// testingMeta.gettestCaseClassName()
 			Class<?> clazz = ucl.loadClass(testingMeta.gettestCaseClassName());
 			TestNG tng = new TestNG();
 			tng.setTestClasses(new Class[] { clazz });
 			TestListenerAdapter listener = new TestListenerAdapter();
 			tng.setOutputDirectory(workPath + "testNG-Log/");
 			tng.addListener(listener);
-			tng.run();
-			result = "PASSED: " + listener.getPassedTests().size() + "\nFAILED: " + listener.getFailedTests().size();
 			
+			log.info("Excute test case");
+			tng.run();
+			result = "PASSED: " + listener.getPassedTests().size()
+					+ "\nFAILED: " + listener.getFailedTests().size();
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return result;
-		
+
 	}
 
 }
