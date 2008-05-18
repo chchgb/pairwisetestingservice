d7e8948b06526c897791ce6dab3a591151bfb5c6
tallshort <tallshort@7176b702-f249-0410-85fa-df573acf1f6a>
2008-05-18 12:47:04 +0000

    Modified test cases for DependencyFinder

--- trunk/SourceCode/Test/src/test/dependency/A.java
+++ trunk/SourceCode/Test/src/test/dependency/A.java
@@ -4,4 +4,13 @@ public class A {
 	B[] b;
 	G g;
 	Object[] obj;
+
+	public void foo() {
+
+		String string = "";
+
+		while (string.endsWith("[]")) {
+			string = string.substring(0, string.length() - 2);
+		}
+	}
 }
